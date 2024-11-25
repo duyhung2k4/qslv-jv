bb612f638e78aa737092fee9258f61244c5de6d1
ndhung <nguyenduyhung04092004@gmail.com>
2024-11-25 20:19:47 +0700

    .

---
+++ src/main/java/mn/qlsv/Qlsv.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mn.qlsv;
+
+/**
+ *
+ * @author HUNG
+ */
+public class Qlsv {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
